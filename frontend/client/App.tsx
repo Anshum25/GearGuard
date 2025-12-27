2f003e89a3a1ee4653a297449010d375ffb6ffab
joy1908 <pateljoy8091@gmail.com>
2025-12-27 12:54:56 +0530

    Get started fix

--- frontend/client/App.tsx
+++ frontend/client/App.tsx
@@ -39,7 +39,7 @@ const App = () => (
                 path="/auth"
                 element={
                   <PublicLayout>
-                    <AuthLanding />
+                    <Signup />
                   </PublicLayout>
                 }
               />
